3ac56f8b48a7b20f8580d10e856aa6e24a62d513
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-06 17:01:20 -0300

    Variables

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args) {
+        
+        int a = 123;
+        long b = 12542145154214L;
+        float c = 12.6f;
+        double d = 3.15;
+        boolean e = true;
+        char symbol = '@';
+        String name = "Evanilson";
+
+        System.out.println("My number is " + a);   
+        System.out.println("My number is " + b);
+        System.out.println("My number is " + c);
+        System.out.println("My number is " + d);
+        System.out.println(e);
+        System.out.println(symbol);
+        System.out.println("My name is " + name);
+    }
+}
