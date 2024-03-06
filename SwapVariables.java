c06e267e60de86c3ab1ade8d7a266f5c03cccb19
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-06 19:36:33 -0300

    Swap two variables

---
+++ SwapVariables.java
@@ -0,0 +1,18 @@
+public class SwapVariables {
+
+    public static void main(String[] args) {
+
+        String x = "water";
+        String y ="Kool-aid";
+        String temp;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(temp);
+
+    }
+}
