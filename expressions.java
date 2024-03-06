3823a435d8936939e0310e4cfe388c3c3c7f7abd
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-06 20:15:27 -0300

    Expressions

---
+++ expressions.java
@@ -0,0 +1,15 @@
+public class expressions {
+    public static void main(String[] args) {
+        // expression = operands and operators
+        // operands = values, variables, numbers, quantity
+        // operators = * - / %
+
+        // int friends = 10;
+        // friends = friends + 1; 
+        // System.err.println(friends); // 11 friends
+
+        double friends = 10;
+        friends = (double) friends / 3;
+        System.out.println(friends); // 3.333333333
+    }
+}
