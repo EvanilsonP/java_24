39ac5f3040e5a50e42e164ac2d0161e7fd8fe2a6
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-08 13:25:28 -0300

    Final keyword

---
+++ finalKeyWor.java
@@ -0,0 +1,10 @@
+public class finalKeyWor {
+    
+    public static void main(String[] args) {
+        // Anything that's final cannot be changed once you declare it
+        final double PI = 3.14159;
+        // PI = 4;
+
+        System.out.println(PI);
+    }
+}
