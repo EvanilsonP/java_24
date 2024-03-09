a44cec98568acbde6abe680a1d3e01ec288f31ff
Evanilson Pereira <pevanilson99@gmail.com>
2024-03-09 11:26:31 -0300

    Variable Scope

--- OOP/constructor.java
+++ OOP/constructor.java
@@ -1,5 +1,7 @@
 package OOP;
 
+import java.util.Random;
+
 public class constructor {
     
     public static void main(String[] args) {
---
+++ OOP/variableScope.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class variableScope {
+    
+    public static void main(String[] args) {
+        // LOCAL VARIABLE = declared inside a method / visible only to tha method
+        // GLOBAL VARIABLE = Declared outisde a method, but within a class / visible to all parts of a class
+        
+        @SuppressWarnings("unused")
+        DiceRoller diceRoller = new DiceRoller();
+    }
+
+}
